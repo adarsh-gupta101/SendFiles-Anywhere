22e58b606ac49963dcec1431620ec89da624d237
adarsh-gupta101 <adarshgupta2468@gmail.com>
2023-10-25 16:30:38 +0530

    port config

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ io.on('connection', (socket) => {
     });
   });
 
-server.listen(3000, () => {
+server.listen(process.env.port || 3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
